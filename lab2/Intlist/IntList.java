fb9c173844bbd39c77f78d642dce17af1a56949d
junfazhu <junfazhu@gmail.com>
2019-08-29 16:30:47 -0700

    change lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,6 +82,9 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if(A == null && B == null){
+            return null;
+        }
         IntList ptr = A;
         while(ptr.rest != null){
             ptr = ptr.rest;
